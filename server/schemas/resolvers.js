e7e472eee4b10c47978c560c54a652722c125aa6
Riley Lum <rileylum@adam.com.au>
2021-10-27 17:55:52 +1030

    add resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,78 @@
-
+const { AuthenticationError } = require('apollo-server-express');
+const { User, Book } = require('../models');
+const {signToken} = require('../utils/auth');
 
 const resolvers = {
+    Query: {
+        user: async(_, {username}) => {
+            return User.findOne({username}).populate('savedBooks')
+        },
 
+        me: async(_, __, context) => {
+            if (context.user) {
+                return User.findOne({_id: context.user._id}).populate('savedBooks')
+            }
+        }
+    },
+
+    Mutation: {
+        login: async(_, {email, password}) => {
+            const user = await User.findOne({email});
+
+            if(!user) {
+                throw new AuthenticationError("Incorrect credentials")
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if(!correctPw) {
+                throw new AuthenticationError("Incorrect credentials")
+            }
+
+            const token = signToken(user);
+
+            return {token, user};
+        },
+
+        addUser: async(_, {username, email, password}) => {
+            const user = await User.create({username, email, password});
+
+            if(!user) {
+                throw new AuthenticationError("Something went wrong! Try again.")
+            }
+            const token = signToken(user);
+            return {token, user};
+        },
+
+        saveBook: async(_, {book: body}, context) => {
+            try {
+                const user = await User.findOneAndUpdate(
+                    {_id: context.user._id},
+                    {$addToSet: {savedBooks: book}},
+                    {new: true, runValidatiors: true}
+                );
+                return user;
+            } catch (err) {
+                console.log(err);
+                throw new AuthenticationError("An unexpected error occured")
+            }
+        },
+
+        removeBook: async(_, {bookId}, context) => {
+            try {
+                const user = await User.findOneAndUpdate(
+                    {_id: context.user._id},
+                    {$pull: {savedBooks: {bookId}}},
+                    {new: true}
+                );
+                return user;
+            } catch (err) {
+                console.log(err);
+                throw new AuthenticationError("An unexpected error occured")
+            }
+        }
+        
+    }
 };
 
 module.exports = resolvers;
