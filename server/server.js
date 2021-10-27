6adb6638c1029870384c561128961f804a981b19
Riley Lum <rileylum@adam.com.au>
2021-10-27 16:35:11 +1030

    setup apollo server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
+
+const {typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
+
 const db = require('./config/connection');
-const routes = require('./routes');
+// OLD REST ROUTES: const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +25,13 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+// OLD REST ROUTES: app.use(routes);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'))
+});
 
-db.once('open', () => {
+db.once('open', async () => {
+  await server.start();
+  server.applyMiddleware({app})
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
